942ba96cd971aac0fceca0e9dc3ce2034ab9835d
Saurabh Saini <saurabhsaini736@gmail.com>
2022-02-18 17:44:21 +0530

    store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import LoginForm from "./components/LoginForm";
+import { Provider } from "react-redux";
+import store from "./redux/Store";
 
 const App = () => {
   return (
-    <>
-      <LoginForm />
-    </>
+    <Provider store={store}>
+      <>
+        <LoginForm />
+      </>
+    </Provider>
   );
 };
 
